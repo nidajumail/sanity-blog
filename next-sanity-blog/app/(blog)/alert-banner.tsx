723ea806139d29cc57e27991086b622a50af845c
nidajumail <nidahaq17@gmail.com>
2025-01-09 04:32:17 +0500

    Update alert-banner.tsx
    second comit

--- next-sanity-blog/app/(blog)/alert-banner.tsx
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -1,5 +1,7 @@
 // "use client";
 
+import Link from "next/link";
+
 // import { useRouter } from "next/navigation";
 // import { useSyncExternalStore, useTransition } from "react";
 
@@ -22,9 +24,12 @@ export default function AlertBanner() {
   return (
     <div
       className=
-      " fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur"
+      " fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur flex justify-center gap-3"
     >
  <div className="py-2 text-center text-lg">Blogs</div>
+ <nav><Link href="/" className=" hover:bg-blue-500 text-green-600">Home</Link>
+ <Link href="/about" className=" hover:bg-blue-500 text-green-600">About</Link>
+ <Link href="/contact" className=" hover:bg-blue-500 text-green-600">Contact</Link></nav>
     </div>
   );
 }
